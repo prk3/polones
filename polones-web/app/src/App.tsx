9c56409a0d57ecb5206ef34bbe50b6516836e7fd
Przemysław Kukulski <kukulski.przemyslaw@gmail.com>
2022-01-16 23:26:36 +0100

    added react app, ejected react-create-app to fix wasm imports

---
+++ polones-web/app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+
+const polones = import('polones-web');
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <span onClick={() => polones.then(module => module.greet())}>Click me</span>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
